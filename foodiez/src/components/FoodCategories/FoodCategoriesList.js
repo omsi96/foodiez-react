1a3e6635695368d05c68627bd2efe05df6fb5d81
Omar <omsi96@users.noreply.github.com>
2020-12-04 12:42:28 +0300

    Done with styling

--- foodiez/src/components/FoodCategories/FoodCategoriesList.js
+++ foodiez/src/components/FoodCategories/FoodCategoriesList.js
@@ -2,17 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import FoodCategoryCard from "./FoodCategoryCard";
 import foodCategories from "../../data/foodCategories";
+import GridWrapper from "../../styles/GridWrapper";
 const FoodCategoriesList = () => {
   // Remove this when connecting to database
 
   return (
     <div>
       <h1>Categories</h1>
-      {foodCategories.map((category) => (
-        <Link to={`/categories/${category.slug}`} key={category.slug}>
-          <FoodCategoryCard category={category} key={category.slug} />
-        </Link>
-      ))}
+      <GridWrapper>
+        {foodCategories.map((category) => (
+          <Link to={`/categories/${category.slug}`} key={category.slug}>
+            <FoodCategoryCard category={category} key={category.slug} />
+          </Link>
+        ))}
+      </GridWrapper>
     </div>
   );
 };
---
+++ foodiez/src/styles/GridWrapper.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const GridWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export default GridWrapper;
